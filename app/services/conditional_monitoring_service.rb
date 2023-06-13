412da6c5d82904554def316a8ec0d24a49be7be4
benetis <git@benetis.me>
2023-06-13 13:38:20 +0300

    feat(conditions) add new triggers

--- app/services/conditional_monitoring_service.rb
+++ app/services/conditional_monitoring_service.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 class ConditionalMonitoringService
+  HOT_TEMP_RANGE = 25...30
+  CLEAR_SKY_HOT_TEMP_RANGE = 24...29
+
+  SCORCHING_TEMP_RANGE = 30..100
+  CLEAR_SKY_SCORCHING_TEMP_RANGE = 29..100
+
+  CLEAR_SKY_RANGE = 0..30
+
   def check_forecasts(time_range, conditions)
     forecasts = Forecast.within_time_range(time_range)
 
@@ -13,6 +21,12 @@ class ConditionalMonitoringService
         case condition
         when :rain
           triggers << { :rain => forecast.forecast_timestamp } if will_rain?(forecast)
+        when :hot
+          triggers << { :hot => forecast.forecast_timestamp } if is_hot?(forecast)
+        when :scorching
+          triggers << { :scorching => forecast.forecast_timestamp } if is_scorching?(forecast)
+        when :windy
+          triggers << { :windy => forecast.forecast_timestamp } if forecast&.wind_gust >= 10
         else
           puts "Unknown condition: #{condition}"
         end
@@ -23,6 +37,24 @@ class ConditionalMonitoringService
 
   def will_rain?(forecast)
     forecast&.total_precipitation&.positive?
+  end
+
+  private
+
+  def temperature_in_range?(forecast, clear_sky_range, other_range)
+    if forecast&.cloud_cover&.in?(CLEAR_SKY_RANGE)
+      forecast&.feels_like_temperature&.in?(clear_sky_range)
+    else
+      forecast&.feels_like_temperature&.in?(other_range)
+    end
+  end
+
+  def is_hot?(forecast)
+    temperature_in_range?(forecast, CLEAR_SKY_HOT_TEMP_RANGE, HOT_TEMP_RANGE)
+  end
+
+  def is_scorching?(forecast)
+    temperature_in_range?(forecast, CLEAR_SKY_SCORCHING_TEMP_RANGE, SCORCHING_TEMP_RANGE)
   end
 
 end
